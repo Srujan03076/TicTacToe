e62755581ed5419a40bee5a4a3f716c12e32d472
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-14 18:13:18 +0530

    Adding UC3 ChooseValidCells program

---
+++ UC3Choosevalidcells.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class UC3Choosevalidcells
+    {
+        public static void createboard()
+        {
+            char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        }
+        public static void chooseletter()
+        {
+            char letter;
+
+            Console.WriteLine("Choose a letter X or O: ");
+            letter = Console.ReadLine()[0];
+
+            if (letter == 'X')
+            {
+                Console.WriteLine("Player's letter is X.");
+                Console.WriteLine("Computer's letter is O.");
+            }
+            else if (letter == 'O')
+            {
+                Console.WriteLine("Player's letter is O.");
+                Console.WriteLine("Computer's letter is X.");
+            }
+            else
+            {
+                Console.WriteLine("Error. You have to choose X or O only.");
+            }
+        }
+        public static void choosevalidcell()
+        {
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("     |     |     ");
+
+
+        }
+
+    }
+}
