bbda6952218363c69258c87e13895af4c39cea57
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-14 18:04:49 +0530

    Adding UC2 ChooseLEtter program

---
+++ UC2ChooseLetter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TicTacToe
+{
+    class UC2Chooseletter
+    {
+        public static void createboard()
+        {
+            char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        }
+        public static void chooseletter()
+        {
+            char letter;
+
+            Console.WriteLine("Choose a letter X or O: ");
+            letter = Console.ReadLine()[0];
+
+            if (letter == 'X')
+            {
+                Console.WriteLine("Player's letter is X.");
+                Console.WriteLine("Computer's letter is O.");
+            }
+            else if (letter == 'O')
+            {
+                Console.WriteLine("Player's letter is O.");
+                Console.WriteLine("Computer's letter is X.");
+            }
+            else
+            {
+                Console.WriteLine("Error. You have to choose X or O only.");
+            }
+        }
+    }
+
+}
